bdda41460439a000c62f576254e0df2f7e85c45c
Alban de La Rochefoucauld <albandelarochefoucauld@air-de-alban.home>
2023-08-21 16:51:29 +0200

    update

---
+++ init.lua
@@ -0,0 +1,21 @@
+require("alban.plugins-setup")
+
+require("alban.core.options")
+require("alban.core.keymaps")
+require("alban.core.colorscheme")
+
+-- PLUGINS
+require("alban.plugins.comment")
+require("alban.plugins.nvim-tree")
+require("alban.plugins.lualine")
+require("alban.plugins.telescope")
+require("alban.plugins.nvim-cmp")
+require("alban.plugins.autopairs")
+require("alban.plugins.treesitter")
+require("alban.plugins.gitsigns")
+
+require("alban.plugins.lsp.mason")
+require("alban.plugins.lsp.lspsaga")
+require("alban.plugins.lsp.lspconfig")
+
+
